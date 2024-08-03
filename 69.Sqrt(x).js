805bc07384bbe82b829d8fb07934f5f5bc95c3c2
salmankalangodan <salman371k@gmail.com>
2024-08-03 15:09:28 +0530

    feat : add ques

---
+++ 69.Sqrt(x).js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(a) {
+    //  var x,
+    //     x1 = a / 2;
+        
+    // do {
+    //     x = x1;
+    //     x1 = (x + (a / x)) / 2;
+    // } while (x !== x1);
+    // return Math.floor(x);
+     return Math.floor(Math.sqrt(a))
+};
