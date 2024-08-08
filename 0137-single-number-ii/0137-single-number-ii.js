c2415f1288d356d8df02e3ff7befaec5865dc5a5
salman_k <159147325+SalmanKalangodan@users.noreply.github.com>
2024-08-08 17:39:47 +0530

    Time: 208 ms (5.02%), Space: 51 MB (50%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -4,19 +4,15 @@
  */
 var singleNumber = function(nums) {
    let count =0
-   const arr = []
    for(i=0; i<nums.length ; i++){
     for(let x of nums){
         if(x === nums[i]){
             count++
           }
     }
-    arr.push(count)
+    if(count ===1){
+        return nums[i]
+    }
     count =0
    } 
-   for(j =0 ; j<arr.length; j++){
-    if(arr[j] === 1){
-        return nums[j]
-    }
-   }
 };
