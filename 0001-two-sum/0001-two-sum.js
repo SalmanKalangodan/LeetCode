99cbce5a370c28755a1a53d74c6c0eb7693aeb7d
salman_k <159147325+SalmanKalangodan@users.noreply.github.com>
2024-08-10 09:30:11 +0530

    Time: 201 ms (5.04%), Space: 49.2 MB (90.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let arr = []
     for (i=0 ; i<nums.length; i++){
         for(j=0 ; j<nums.length; j++){
-            if(nums[i]+nums[j] === target && i !==j){
-                 arr.push(i, j)
-                 return arr
+            if(nums[i]+nums[j] === target && i !==j){ 
+                 return [i,j]
             }
         }
     }
