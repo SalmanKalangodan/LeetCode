c9a499ebeded73c734149a8299361e693ae1cd99
salman_k <159147325+SalmanKalangodan@users.noreply.github.com>
2024-08-21 12:16:41 +0530

    Time: 53 ms (47.12%), Space: 49 MB (23.61%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let arr = s.split(" ")
+      if(pattern.length !== arr.length) return false
+    const a= new Map()
+    const b = new Map()
+ 
+    for(i=0; i<pattern.length; i++){
+       let pe = pattern[i]
+       let se= arr[i]
+       if(a.has(pe)&&a.get(pe) !== se){
+        return false
+       }
+       if(b.has(se) && b.get(se) !== pe){
+        return false
+       }
+       a.set(pe , se)
+       b.set(se , pe)
+    }
+    return true
+};
